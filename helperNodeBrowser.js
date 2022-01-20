9a78c480b154275d52d0ce7ec17f8f0afcfa3957
Julian Gonzalez <joticajulian@gmail.com>
2022-01-20 22:52:03 +0100

    build for browser using fetch - #8

---
+++ helperNodeBrowser.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const [type] = process.argv.slice(2);
+const filename = "./src/Provider.ts";
+let providerData = fs.readFileSync(filename, "utf-8");
+
+const lines = providerData.split(/\n/);
+if (type === "--browser") {
+  if (lines[0].includes("import fetch"))
+    providerData = lines.slice(1).join("\n");
+} else if (type === "--node") {
+  if (!lines[0].includes("import fetch"))
+    providerData = `import fetch from "cross-fetch";\n${providerData}`;
+} else {
+  throw new Error(`Invalid type ${type}`);
+}
+
+fs.writeFileSync(filename, providerData, "utf-8");
