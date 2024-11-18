35dd8f765ad4f2dd4abbb591e3ec8f10e6432df5
Julian Gonzalez <joticajulian@gmail.com>
2024-11-18 18:11:42 +0100

    use ProviderInterface other classes

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
 import { sha256 } from "@noble/hashes/sha256";
 import { Contract } from "./Contract";
-import { Provider } from "./Provider";
+import { ProviderInterface } from "./Provider";
 import { SignerInterface } from "./Signer";
 import {
   Abi,
@@ -90,7 +90,7 @@ export class Transaction {
   /**
    * Provider to connect with the blockchain
    */
-  provider?: Provider;
+  provider?: ProviderInterface;
 
   /**
    * Transaction
@@ -109,7 +109,7 @@ export class Transaction {
 
   constructor(c?: {
     signer?: SignerInterface;
-    provider?: Provider;
+    provider?: ProviderInterface;
     transaction?: TransactionJson;
     options?: TransactionOptions;
   }) {
@@ -245,7 +245,7 @@ export class Transaction {
    */
   static async prepareTransaction(
     tx: TransactionJson,
-    provider?: Provider,
+    provider?: ProviderInterface,
     payer?: string
   ): Promise<TransactionJson> {
     if (!tx.header) {
